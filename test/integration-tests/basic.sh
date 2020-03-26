db8ba5bd47234a4d2f4641de4a593acdbbdb62db
Patric Jansson <paddy@kth.se>
2020-03-26 16:37:58 +0100

    Example

---
+++ test/integration-tests/basic.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+error() { printf "\n \033[0;31mERROR: $@\033[0;0m | $(date) \n"; }
+passed() { printf "\n \033[0;32m   OK: $@\033[0;0m | $(date) \n\n"; }
+
+MONITOR_URL="http://web:3000/node-api/_monitor";
+
+PATTERN="APPLICATION_STATUS: OK" #env in set in file: docker-compose-integration-tests.yml
+
+sleep 5s
+
+RESPONSE="$(curl -s -S --max-time 30 $MONITOR_URL)"
+
+if [[ $RESPONSE != *$PATTERN* ]]; then
+    error "URL '$MONITOR_URL' does not contain '$PATTERN'."
+    exit 1
+fi
+
+passed "Basic test passed for '$MONITOR_URL'."
+    
+exit 0
